b55262ee26bdfd7e4137ee6acae669a4fab7d67f
Gabacles <g.dalrovere@gmail.com>
2025-03-18 16:51:27 -0300

    feat: add DTOs for login and registration responses with access token

---
+++ src/auth/dtos/register-response.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { AccessToken } from '../types/access-token';
+import { User } from 'src/user/entities/user.entity';
+
+export class RegisterResponseDTO extends PartialType(User) {
+  access_token: AccessToken;
+}
