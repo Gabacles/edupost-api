04d2fa25ebca89ff8fb921e18ef6a9972a11e8aa
Gabacles <g.dalrovere@gmail.com>
2025-03-17 23:13:38 -0300

    feat: update PostController to include author ID in post creation and updates, and rename methods for clarity

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Param, Put, Delete, Get, Request } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 
-@Controller('post')
+@Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
+  async createPost(
+    @Body() createPostDto: CreatePostDto,
+    @Request() req: any,
+  ) {
+    const authorId = req.user.id;
+    return this.postService.create(createPostDto, authorId);
   }
 
   @Get()
-  findAll() {
+  async findAllPosts() {
     return this.postService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
+  async viewPost(@Param('id') id: number) {
+    return this.postService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
+  @Put(':id')
+  async updatePost(
+    @Param('id') id: number,
+    @Body() updatePostDto: UpdatePostDto,
+    @Request() req: any,
+  ) {
+    const authorId = req.user.id;
+    return this.postService.update(id, updatePostDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
+  async removePost(@Param('id') id: number) {
+    return this.postService.remove(id);
   }
 }
