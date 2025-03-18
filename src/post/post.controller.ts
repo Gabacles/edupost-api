13e6aa6c704306cb0f1c20c7851cf27442ff34b2
Gabacles <g.dalrovere@gmail.com>
2025-03-18 16:52:14 -0300

    feat: add role-based access control for post creation, update, and deletion

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, Post, Body, Param, Put, Delete, Get, Request } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  Get,
+  Request,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @Roles('TEACHER')
   @Post()
-  async createPost(
-    @Body() createPostDto: CreatePostDto,
-    @Request() req: any,
-  ) {
+  async createPost(@Body() createPostDto: CreatePostDto, @Request() req: any) {
     const authorId = req.user.id;
     return this.postService.create(createPostDto, authorId);
   }
@@ -26,6 +34,7 @@ export class PostController {
     return this.postService.findOne(id);
   }
 
+  @Roles('TEACHER')
   @Put(':id')
   async updatePost(
     @Param('id') id: number,
@@ -36,6 +45,7 @@ export class PostController {
     return this.postService.update(id, updatePostDto);
   }
 
+  @Roles('TEACHER')
   @Delete(':id')
   async removePost(@Param('id') id: number) {
     return this.postService.remove(id);
