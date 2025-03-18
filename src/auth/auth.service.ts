dc128e7c8e827b77936847453673a023f630934f
Gabacles <g.dalrovere@gmail.com>
2025-03-18 16:48:50 -0300

    feat: implement AuthService for user authentication and registration

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare, hash } from 'bcryptjs';
+import { UserService } from 'src/user/user.service';
+import { User } from 'src/user/entities/user.entity';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.userService.findOneByEmail(email);
+    if (!user) {
+      throw new BadRequestException('invalid credentials');
+    }
+
+    const isMatch = await compare(password, user.password);
+    if (!isMatch) {
+      throw new BadRequestException('invalid credentials');
+    }
+
+    return user;
+  }
+
+  async login(user: User): Promise<{ access_token: string }> {
+    console;
+    const payload = { email: user.email, id: user.id, roles: user.roles };
+    return { access_token: this.jwtService.sign(payload) };
+  }
+
+  async register(createUserDto: CreateUserDto): Promise<any> {
+    const existingUser = await this.userService.findOneByEmail(
+      createUserDto.email,
+    );
+    if (existingUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const hashedPassword = await hash(createUserDto.password, 10);
+    const newUser = await this.userService.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    return this.login(newUser);
+  }
+}
