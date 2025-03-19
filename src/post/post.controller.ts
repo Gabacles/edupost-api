8830f83996b511350433951c06681c53fd6e319e
Gabacles <g.dalrovere@gmail.com>
2025-03-19 14:47:58 -0300

    feat: enhance PostController with Swagger documentation for improved API clarity

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -14,19 +14,27 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { Roles } from '@/common/decorators/roles.decorator';
 import { IPost } from './models/post.interface';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Roles('TEACHER')
   @Post()
+  @ApiOperation({ summary: 'Create post' })
   async createPost(@Body() createPostDto: CreatePostDto, @Request() req: any) {
     const authorId = req.user.id;
     return this.postService.create(createPostDto, authorId);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Find all posts' })
+  @ApiOkResponse({
+    description: 'List of all posts',
+    type: [CreatePostDto],
+  })
   async findAllPosts(
     @Query('limit') limit: number,
     @Query('page') page: number,
@@ -35,6 +43,7 @@ export class PostController {
   }
 
   @Get('search')
+  @ApiOperation({ summary: 'Search posts by title or content' })
   async searchPosts(
     @Query('query') query: string,
     @Query('limit') limit?: number,
@@ -44,12 +53,14 @@ export class PostController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find post by id' })
   async viewPost(@Param('id') id: number) {
     return this.postService.findOne(id);
   }
 
   @Roles('TEACHER')
   @Put(':id')
+  @ApiOperation({ summary: 'Update post by id' })
   async updatePost(
     @Param('id') postId: number,
     @Body() updatePostDto: UpdatePostDto,
@@ -61,6 +72,7 @@ export class PostController {
 
   @Roles('TEACHER')
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete post by id' })
   async removePost(@Param('id') id: number) {
     return this.postService.remove(id);
   }
