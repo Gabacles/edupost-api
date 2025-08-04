51834c27c4236bce8d11d65dd31d541fd99f1387
Gabacles <g.dalrovere@gmail.com>
2025-08-04 20:26:48 -0300

    feat: enable CORS for the application to allow cross-origin requests

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Edupost API')
     .setDescription(
